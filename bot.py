ed937b030ccf806b9231c5ddd6600a6ecbad93c1
Makism <123948424+Mixerka1@users.noreply.github.com>
2026-09-06 02:08:29 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 # В Railway: Project -> Variables -> добавить BOT_TOKEN = твой токен от BotFather.
 # Для локального теста можно временно раскомментировать строку ниже и вписать токен напрямую:
 # BOT_TOKEN = "123456789:AAExxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-   BOT_TOKEN = os.environ["BOT_TOKEN"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
 
 logging.basicConfig(level=logging.INFO)
 
